c66032a3b20a041cbf2c2f73c3cdb914f0fb9e94
James Lee <j584lee98@gmail.com>
2025-12-18 13:24:08 -0500

    feat: update tool calling functions in mcp client

--- backend/mcp_client.py
+++ backend/mcp_client.py
@@ -61,9 +61,44 @@ async def call_slack_tool(tool_name: str, arguments: dict):
 async def list_google_drive_files(token: str, folder_id: str = 'root'):
     return await call_google_drive_tool("list_files", {"token": token, "folder_id": folder_id})
 
+async def search_google_drive_files(token: str, query: str):
+    return await call_google_drive_tool("search_files", {"token": token, "query": query})
+
+async def read_google_drive_file(token: str, file_id: str):
+    return await call_google_drive_tool("read_file_content", {"token": token, "file_id": file_id})
+
+# GitHub Tools
+async def list_github_repos(token: str):
+    return await call_github_tool("list_repos", {"token": token})
+
+async def list_github_branches(token: str, repo_full_name: str):
+    return await call_github_tool("list_branches", {"token": token, "repo_full_name": repo_full_name})
+
+async def list_github_pull_requests(token: str, repo_full_name: str, state: str = "open"):
+    return await call_github_tool("list_pull_requests", {"token": token, "repo_full_name": repo_full_name, "state": state})
+
+async def list_github_issues(token: str, repo_full_name: str, state: str = "open"):
+    return await call_github_tool("list_issues", {"token": token, "repo_full_name": repo_full_name, "state": state})
+
+async def get_github_file_content(token: str, repo_full_name: str, file_path: str, ref: str = None):
+    args = {"token": token, "repo_full_name": repo_full_name, "file_path": file_path}
+    if ref:
+        args["ref"] = ref
+    return await call_github_tool("get_file_content", args)
+
+# Slack Tools
 async def list_slack_channels(token: str):
     return await call_slack_tool("list_channels", {"token": token})
 
+async def list_slack_users(token: str):
+    return await call_slack_tool("list_users", {"token": token})
+
 async def get_slack_channel_history(token: str, channel_id: str):
     return await call_slack_tool("get_channel_history", {"token": token, "channel_id": channel_id})
+
+async def get_slack_thread_replies(token: str, channel_id: str, thread_ts: str):
+    return await call_slack_tool("get_thread_replies", {"token": token, "channel_id": channel_id, "thread_ts": thread_ts})
+
+async def search_slack_messages(token: str, query: str):
+    return await call_slack_tool("search_messages", {"token": token, "query": query})
 
